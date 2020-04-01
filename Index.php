129542089d6e25f858d3a66ff119f439d5cd04cb
Theovady Moutty <theomoutty@gmail.com>
2020-04-01 09:09:56 +0200

    initial commit

---
+++ Index.php
@@ -0,0 +1,323 @@
+<html lang="fr">
+    <head>
+    <link rel="stylesheet" href="php.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Projet php site vitrine</title>
+</head>
+    <body>
+        <body>
+            <header>
+                <nav>
+                    <div class="menu">
+                        <p>
+                            <a href="#exo" class="cara">Story</a>
+                        </p>
+                        <p>
+                            <a href="#Algorithme" class="cara">Algorithme</a>
+                        </p>
+                        <p>
+                            <a href="#BDD" class="cara">Base de donnée</a> </p>
+                        <p>
+                            <a href="#Laposte" class="cara">La poste</a>
+                        </p>
+                    </div>
+                </nav>
+            </header>
+            
+            <div id="sto">
+                <div class="title">
+                    <h1>Story</h1>
+                    <p>La section story nous permet de lister nos story ; faites / en cours / a faire : 
+                        ce projet a ete fait avec : Anthony Bac, Anthony Pascal , Theovady Moutty
+                    </p>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 1</h5>
+                    <p>En tant que utilisateur<br>
+                        je veux Pouvoir Indique sur la page d'accueil les membres de l'équipe 
+                        et le nombre de story terminés, lister, la liste des us terminer et non terminer<br>
+                        Afin de permettre au po de rapidement savoir ce qui a pose probleme et ce qui a ete reussi
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 2</h5>
+                    <p>En tant que developpeur<br>
+                        Je veux pouvoir reutiliser dans toutes les pages de mon site 
+                        le meme header, footer afin de ne recreer que le contenu body 
+                        dans les differentes page<br>
+                        Afin de simplifier la maintenance future
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 3</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la page Exercice un formulaire me demandant de saisir un nombre<br>
+                        Afin de m'indiquer tous les nombres premiers jusqu'a ce nombre
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 4</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la page Exercice indiquer a l'aide d'un formulaire
+                        un nombre de seconde<br>
+                        Afin de connaitre la data a laquelle a eu lieu l'evenemnt.
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 5</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir saisir 3 nombres dans un formulaire sur la page Exercice<br>
+                        Afin de Calculer le plus petit nombre parmis les trois saisis
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 6</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la page Exercice pouvoir indiquerdans un formulaireun nombre<br>
+                        Afin de connaitre sa valeur en chiffre Romain</p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 7</h5>
+                    <p>En tant que utilisateur <br>
+                        Je veux pouvoir dans la page Exercice saisir via un formulaire un nombre<br>
+                        Afin de Calculer sa factorielle
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 8</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la page Exercice dans un formulaire saisir un nombre<br>
+                        Afin de convertir cette valeur en HexaDecimale
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 9</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la page Exercice renseigner un nombre dans un formulaire dedié<br>
+                        Afin de le convertir en binaire
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 10</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans l'onglet Exercice disposer d'un formulaire ou je puisse saisir mon adresse mail et ma date de naissance<br>
+                        Afin de savoir si je les ai bien rediges
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 11</h5>
+                    <p>En tant que utilisateur <br>
+                        Je veux pouvoir saisir dans la page exercice une liste de prenoms separe par des virgules<br>
+                        Afin de Trier un tableau de nom a partir du second caractére
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 12</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir saisir dans la page MesContacts les informations de mes contacts et pouvoir les conserver indefiniment<br>
+                        Afin de pouvoir garder en memoire tout mes contacts
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 13</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la mage Mes Contacts afficher tous mes contacts precedemment saisi<br>
+                        Afin de pouvoir avoir une vision globale de mon carnet d'adresse
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 14</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir Modifier un contact saisi precedement <br>
+                        Afin de corriger une saisie erronée
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 15</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir dans la page La Poste disposer d'un formulaire<br>
+                        Afin de pouvoir saisir toutes les informationspour obtenir le patron d'une enveloppe pre rempli
+                        avec les informations de mon contact
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+                <div class ="stor">
+                    <div id="story">
+                    <h5>Story 16</h5>
+                    <p>En tant que utilisateur<br>
+                        Je veux pouvoir lors de la validation de mon formulaire de la page Laposte voir ma lettre genere en fonction des informationssaisiers
+                        dans le formulaire<br>
+                        Afin de pouvoir avoir un preview de ma lettre et eventuellement l'imprimer comme un patron
+                    </p>
+                    </div>
+                    <div id = "zubi">
+                      <p>Fait ou pas fait</p>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            
+            <div id="Algorithme">
+                <div class="title">
+                    <h1>Algorithme</h1>
+                    <p>Voici la page des Algorithme.</p>
+                </div>
+                <hr>
+                <div class ="exo1">
+                    <h5>Exercice 1</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Nunc bibendum lectus ac pellentesque viverra. In sit amet facilisis tortor.
+                      Etiam venenatis odio a ipsum aliquam ultricies. Integer quis purus erat. Nullam 
+                      pretium convallis eros, vel scelerisque nisi feugiat ut. Duis vitae augue ultrices, 
+                      ultricies ex sit amet, venenatis tortor. Aenean in pharetra sem. Quisque euismod 
+                      fringilla vehicula. Curabitur tristique orci vel lacus blandit molestie. Suspendisse 
+                      aliquet nibh a egestas gravida. Nullam quis massa gravida, consectetur ipsum a, 
+                      laoreet nunc. Vestibulum molestie arcu mi, sit amet mollis orci gravida eget. 
+                      Fusce id velit mollis, faucibus lectus nec, sagittis lorem. Fusce imperdiet 
+                      dui dolor, ut dictum lectus elementum et. Ut non luctus ipsum. Vestibulum 
+                      rhoncus sed dui at elementum.</p>
+                </div>
+                <hr>
+            </div>
+
+            <div id="BDD">
+                <div class="title">
+                    <h1>Base de donnée</h1>
+                    <p>Voici la page de la base de donnée.</p>
+                </div>
+                <hr>
+                <div class ="exo1">
+                    <h5>Exercice 1</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Nunc bibendum lectus ac pellentesque viverra. In sit amet facilisis tortor.
+                      Etiam venenatis odio a ipsum aliquam ultricies. Integer quis purus erat. Nullam 
+                      pretium convallis eros, vel scelerisque nisi feugiat ut. Duis vitae augue ultrices, 
+                      ultricies ex sit amet, venenatis tortor. Aenean in pharetra sem. Quisque euismod 
+                      fringilla vehicula. Curabitur tristique orci vel lacus blandit molestie. Suspendisse 
+                      aliquet nibh a egestas gravida. Nullam quis massa gravida, consectetur ipsum a, 
+                      laoreet nunc. Vestibulum molestie arcu mi, sit amet mollis orci gravida eget. 
+                      Fusce id velit mollis, faucibus lectus nec, sagittis lorem. Fusce imperdiet 
+                      dui dolor, ut dictum lectus elementum et. Ut non luctus ipsum. Vestibulum 
+                      rhoncus sed dui at elementum.</p>
+                </div>
+                <hr>
+            </div>
+
+            <div id="Laposte">
+                <div class="title">
+                    <h1>La poste</h1>
+                    <p>Voici la page de la poste.</p>
+                </div>
+                <hr>
+                <div class ="exo1">
+                    <h5>Exercice 1</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Nunc bibendum lectus ac pellentesque viverra. In sit amet facilisis tortor.
+                      Etiam venenatis odio a ipsum aliquam ultricies. Integer quis purus erat. Nullam 
+                      pretium convallis eros, vel scelerisque nisi feugiat ut. Duis vitae augue ultrices, 
+                      ultricies ex sit amet, venenatis tortor. Aenean in pharetra sem. Quisque euismod 
+                      fringilla vehicula. Curabitur tristique orci vel lacus blandit molestie. Suspendisse 
+                      aliquet nibh a egestas gravida. Nullam quis massa gravida, consectetur ipsum a, 
+                      laoreet nunc. Vestibulum molestie arcu mi, sit amet mollis orci gravida eget. 
+                      Fusce id velit mollis, faucibus lectus nec, sagittis lorem. Fusce imperdiet 
+                      dui dolor, ut dictum lectus elementum et. Ut non luctus ipsum. Vestibulum 
+                      rhoncus sed dui at elementum.</p>
+                </div>
+                <hr>
+            </div>
+            
+    </body>
+    
+</html>
