aef78bce7f32950ad99c79884ed56a01b434fdb9
Theovady Moutty <theomoutty@gmail.com>
2020-04-01 10:27:50 +0200

    add footer and story 5

--- Index.php
+++ Index.php
@@ -319,5 +319,11 @@
             </div>
             
     </body>
+    <section class="footer">
+    <a type="button" href="https://twitter.com/idcsurinternet" target="_blank">Twitter</a>
+    <a type="button" href="https://www.facebook.com/anthony.pascal.5" target="_blank">Facebook</a>
+    <a type="button" href="https://www.instagram.com/pnthonyaascal" target="_blank">Instagram</a>
+    <div style="font-size: 9pt; color: #817D7C" class="push">© Copyright 2020 Tous droits réservés</div>
+</section>
     
 </html>
