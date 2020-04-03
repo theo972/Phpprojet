428274b77f9f32e24d923913b3a350a14927760d
Theovady Moutty <theomoutty@gmail.com>
2020-04-03 02:26:16 +0200

    add footer and header

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+<footer>
+    <a type="button" href="https://twitter.com/idcsurinternet" target="_blank">Twitter</a>
+    <a type="button" href="https://www.facebook.com/anthony.pascal.5" target="_blank">Facebook</a>
+    <a type="button" href="https://www.instagram.com/pnthonyaascal" target="_blank">Instagram</a>
+    <div style="font-size: 9pt; color: #817D7C" class="push">© Copyright 2020 Tous droits réservés</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,16 @@
+
+<header>
+    <nav>
+    <div class="menu">
+        <p>
+            <a href="#exo" class="cara">Story</a>
+        </p>
+        <p>
+        <a href="#Algorithme" class="cara">Algorithme</a>
+        </p>
+        <p>
+        <a href="#Laposte" class="cara">La poste</a>
+        </p>
+    </div>
+    </nav>
+</header>
